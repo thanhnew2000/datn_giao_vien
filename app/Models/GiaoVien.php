8d13a0e287d9224618ca0ad099a3ed3f6432b1be
unknown <quanglxph07563@fpt.edu.vn>
2020-11-07 10:43:37 +0700

    'quanglx-add-thong-bao-tin-tuc'

--- app/Models/GiaoVien.php
+++ app/Models/GiaoVien.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\LopHoc;
 class GiaoVien extends Model
 {
     protected $table = "giao_vien";
+
+    public function Lop()
+    {
+        return $this->belongsTo(LopHoc::class,'lop_id','id');
+    }
 }
+
+
